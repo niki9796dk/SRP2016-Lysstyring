19a8f2cda4b3547f07b9dcf921a75ccdb2c7d1d6
niki9796dk <nikiewald@hotmail.dk>
2016-12-20 03:40:03 +0100

    Update readState.php

--- PHP/readState.php
+++ PHP/readState.php
@@ -1,20 +1,27 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Denne fil har til formål at læse det nuværende lysstadie og gemme daten til senere brug.
 
 //Settings:
+
+// Gemmer lokalisationen af API'en
 $stateDic = "C:\Users\KFU24KLMP\Dropbox\Public/arduino.txt";
 
 //Setup:
+
+// Åbner API'en til læsning
 $stateFile = fopen($stateDic, "r");
 
 //Program:
+
+// Læs API'en og gem indholdet som rå data
 $stateRAW = fread($stateFile, filesize($stateDic));
+
+// Adskil og læs dataen i variablen $stateRaw
 $state = explode(":", $stateRAW);
+
+// Gem hvor mange LED'er der kontrolleres
 $LEDCount = count($state);
 
+// Luk API'en for at spare på hukommelsen
 fclose($stateFile);
